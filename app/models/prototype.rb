1f444894a0cc1607f3c9f7576d093aa66a393ee9
yyyukina <138771405+yyyukina@users.noreply.github.com>
2023-07-28 18:34:22 +0900

    修正依頼＆データベース設定変更

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,15 +1,17 @@
 class Prototype < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy
-    has_one_attached :image_presence
+    has_one_attached :image
     
     validates :title, presence: true
     validates :catch_copy, presence: true
     validates :concept, presence: true
 
-    def image_presence
+    validate :validate_image_presence
+
+    def validate_image_presence
         unless image.attached?
+         errors.add(:image)
+        end
     end
-
-
 end

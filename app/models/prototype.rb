8bcb5301ddca918eb71c6cb944e1a6117c0315cc
yyyukina <138771405+yyyukina@users.noreply.github.com>
2023-07-28 15:20:32 +0900

    「画像は1枚必須」修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,11 +1,15 @@
 class Prototype < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy
-    has_one_attached :image
+    has_one_attached :image_presence
     
     validates :title, presence: true
     validates :catch_copy, presence: true
     validates :concept, presence: true
+
+    def image_presence
+        unless image.attached?
+    end
 
 
 end
